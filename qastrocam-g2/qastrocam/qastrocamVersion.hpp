683792315a2b2bd3c9fe4ca96f59d246b04f2d6a
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-08-27 04:24:23 +0000

    Updated with the sourceforge infos

--- qastrocam-g2/qastrocam/qastrocamVersion.hpp
+++ qastrocam-g2/qastrocam/qastrocamVersion.hpp
@@ -2,9 +2,9 @@
 #define _qastrocamVersion_hpp_
 
 const char * const qastroCamVersion ="4.2.2 beta BFC Fork";
-const int          qastrocamBuild   =3;
-const char * const qastrocamWeb = "no site at this time";
-const char * const qastrocamMail = "no mail at this time";
-const char * const qastrocamName = "QAstroCam BFC Fork";
+const int          qastrocamBuild   =4;
+const char * const qastrocamWeb = "http://sourceforge.net/projects/qastrocam-g2/";
+const char * const qastrocamMail = "thx8411@users.sourceforge.net";
+const char * const qastrocamName = "QAstrocam-g2";
 
 #endif
