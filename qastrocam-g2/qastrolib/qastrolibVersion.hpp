8abf99761e247ba0a87f0b30a3ba245db4833d8a
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-08-28 03:20:21 +0000

    qastrolib vers. updated

--- qastrocam-g2/qastrolib/qastrolibVersion.hpp
+++ qastrocam-g2/qastrolib/qastrolibVersion.hpp
@@ -1,10 +1,10 @@
 #ifndef _qastrolibVersion_hpp_
 #define _qastrolibVersion_hpp_
 
-const char * const qastrolibVersion ="Pre 4";
-const int          qastrolibBuild   =32;
-const char * const qastrolibWeb = "http://3demi.net/astro/qastrocam/doc/";
-const char * const qastrolibMail = "qastrolib@3demi.net";
-const char * const qastrolibName = "QAstroLib";
+const char * const qastrolibVersion ="4.2.2beta";
+const int          qastrolibBuild   =1;
+const char * const qastrolibWeb = "http://sourceforge.net/projects/qastrocam-g2/";
+const char * const qastrolibMail = "thx8411@users.sourceforge.net";
+const char * const qastrolibName = "QastroLib-g2";
 
 #endif
