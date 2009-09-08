20c3e5c0414eabe3bb32a5a5f2ab77e135c4d05f
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-09-08 23:53:52 +0000

    Working on settings backup, adding tests sets

--- trunk/qastrocam-g2/qastrolib/SettingsBackup.hpp
+++ trunk/qastrocam-g2/qastrolib/SettingsBackup.hpp
@@ -1,9 +1,24 @@
+#ifndef _SETTINGSBACKUP_H_
+#define _SETTINGSBACKUP_H_
+
+#include <string>
+
+using namespace std;
+
+#define NB_RECORDS	16
 
 class settingsBackup {
 	private :
+		string fileName;
+		string datas[NB_RECORDS][2];
+		int findKey(const char* key);
+		void addKey(const char* key, const char* val);
 		void serialize();
 	public :
-		settingsBackup() {};
-		~settingsBackup() {};
+		settingsBackup();
+		void setKey(const char* key, const char* val);
+		const char* getKey(const char* key);
 		void deSerialize();
 };
+
+#endif
