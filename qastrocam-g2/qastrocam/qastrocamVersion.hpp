a35eff5dc183c34bce400e34b6a37b17412739fd
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-09-08 14:40:19 +0000

    Changing version

--- qastrocam-g2/qastrocam/qastrocamVersion.hpp
+++ qastrocam-g2/qastrocam/qastrocamVersion.hpp
@@ -1,8 +1,8 @@
 #ifndef _qastrocamVersion_hpp_
 #define _qastrocamVersion_hpp_
 
-const char * const qastroCamVersion ="4.2rc2";
-const int          qastrocamBuild   =15;
+const char * const qastroCamVersion ="4.3beta20090908";
+const int          qastrocamBuild   =16;
 const char * const qastrocamWeb = "http://sourceforge.net/projects/qastrocam-g2/";
 const char * const qastrocamMail = "thx8411@users.sourceforge.net";
 const char * const qastrocamName = "Qastrocam-g2";
