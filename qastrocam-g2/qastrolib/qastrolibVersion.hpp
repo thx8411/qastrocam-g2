aad35b435621d1a4e889f415270706632e3cd327
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-09-08 12:23:58 +0000

    Version name changed

--- qastrocam-g2/qastrolib/qastrolibVersion.hpp
+++ qastrocam-g2/qastrolib/qastrolibVersion.hpp
@@ -1,8 +1,8 @@
 #ifndef _qastrolibVersion_hpp_
 #define _qastrolibVersion_hpp_
 
-const char * const qastrolibVersion ="4.2.2.rc2";
-const int          qastrolibBuild   =8;
+const char * const qastrolibVersion ="4.2rc2";
+const int          qastrolibBuild   =9;
 const char * const qastrolibWeb = "http://sourceforge.net/projects/qastrocam-g2/";
 const char * const qastrolibMail = "thx8411@users.sourceforge.net";
 const char * const qastrolibName = "QastroLib-g2";
