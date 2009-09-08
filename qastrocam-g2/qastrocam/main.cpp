62c5925ce39cbfcc23d07b0430b8331be660724b
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-09-08 00:58:03 +0000

    Installing lib is useless, this lib is static

--- qastrocam-g2/qastrocam/main.cpp
+++ qastrocam-g2/qastrocam/main.cpp
@@ -400,7 +400,7 @@ int main(int argc, char ** argv) {
    }
    QCamUtilities::setQastrocamIcon(&mainWindow);
 
-   mainWindow.move(0,0);
+   //mainWindow.move(0,0);
    mainWindow.show();
    mainWindow.adjustSize();
 
