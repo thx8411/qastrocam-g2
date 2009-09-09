18f0aca398d7e1a214ba3d9ab2d12a47dcd77d1f
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-09-09 00:37:48 +0000

    Tests Makefile added

--- qastrocam-g2/qastrolib/SettingsBackup.cpp
+++ qastrocam-g2/qastrolib/SettingsBackup.cpp
@@ -6,15 +6,27 @@ using namespace std;
 int settingsBackup::findKey(const char* key) {
 	int i=0;
 
+#ifdef _DEBUG_
+        cout << "finding key : '" << key << "' position : ";
+#endif
 	while((i<NB_RECORDS)&&(datas[0][i]!=key)) i++;
 	if(i==NB_RECORDS) i=-1;
+#ifdef _DEBUG_
+        cout << i << endl;
+#endif
 	return(i);
 }
 
 void settingsBackup::addKey(const char* key, const char* val) {
 	int i;
 
+#ifdef _DEBUG_
+        cout << "adding key : '" << key << "' value : '" << val << "' position : ";
+#endif
 	while((i<NB_RECORDS)&&(datas[0][i]!="")) i++;
+#ifdef _DEBUG_
+	cout << i << endl;
+#endif
 	if(i==NB_RECORDS) cout << "Settings tab full\n";
 	else {
 		datas[0][i]=key;
@@ -26,7 +38,9 @@ void settingsBackup::serialize() {
 	FILE* fd;
 	int i=0;
 
-	//cout << "Writing settings\n";
+#ifdef _DEBUG_
+	cout << "Writing settings\n";
+#endif
 	fd=fopen(fileName.c_str(),"w");
 	if(fd!=NULL) {
 		while(datas[0][i]!="") {
@@ -35,6 +49,12 @@ void settingsBackup::serialize() {
 		}
 		fclose(fd);
 	}
+#ifdef _DEBUG_
+	else {
+		perror(" file error : ");
+	}
+	cout << "writing " << i << " entries\n";
+#endif
 }
 
 settingsBackup::settingsBackup() {
@@ -64,10 +84,12 @@ void settingsBackup::deSerialize() {
 	char key[256];
 	char value[256];
 
-	//cout << "Loading settings\n";
+#ifdef _DEBUG_
+	cout << "Loading settings\n";
+#endif
 	fd=fopen(fileName.c_str(),"r");
 	if(fd!=NULL) {
-		while(feof(fd)) {
+		while(!feof(fd)) {
 			fscanf(fd,"%s",key);
 			fscanf(fd,"%s",value);
 			datas[0][i]=key;
@@ -76,5 +98,11 @@ void settingsBackup::deSerialize() {
 		}
 		fclose(fd);
 	}
+#ifdef _DEBUG_
+	else {
+                perror("file error");
+        }
+	cout << "reading " << i << " entries\n"; 
+#endif
 }
 
