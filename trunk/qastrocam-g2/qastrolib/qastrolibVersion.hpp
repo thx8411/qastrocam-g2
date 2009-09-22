a5494193cfb096dcb256f2688a0cd9473b402a46
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-09-22 00:35:55 +0000

    updated to 4.3rc1

--- trunk/qastrocam-g2/qastrolib/qastrolibVersion.hpp
+++ trunk/qastrocam-g2/qastrolib/qastrolibVersion.hpp
@@ -1,8 +1,8 @@
 #ifndef _qastrolibVersion_hpp_
 #define _qastrolibVersion_hpp_
 
-const char * const qastrolibVersion ="4.3beta20090922";
-const int          qastrolibBuild   =18;
+const char * const qastrolibVersion ="4.3rc1";
+const int          qastrolibBuild   =19;
 const char * const qastrolibWeb = "http://sourceforge.net/projects/qastrocam-g2/";
 const char * const qastrolibMail = "thx8411@users.sourceforge.net";
 const char * const qastrolibName = "QastroLib-g2";
