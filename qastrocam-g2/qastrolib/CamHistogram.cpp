104dc2cc1587647ad2b135b6563c2731dbce619e
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-10-04 04:02:36 +0000

    removed useless code

--- qastrocam-g2/qastrolib/CamHistogram.cpp
+++ qastrocam-g2/qastrolib/CamHistogram.cpp
@@ -12,7 +12,6 @@
 #include <qvgroupbox.h>
 #include <qpushbutton.h>
 #include <qprogressbar.h>
-//#include <qlcdnumber.h>
 #include <qlabel.h>
 #include <qhgroupbox.h>
 #include <qhbox.h>
@@ -26,9 +25,7 @@ CamHistogram::CamHistogram(QCam & theCam) :
    QCamClient(theCam) {
    mainWindow_ = new QHGroupBox(tr("Analyse"));
    QCamUtilities::setQastrocamIcon(mainWindow_);
-   //cam_ = cam;
-   //connect(cam_,SIGNAL(newFrame()),this,SLOT(updateDisplay()));
-   
+
    for (int i=0;i<256;++i) {
       histogram_[i]=0;
    }
@@ -41,29 +38,26 @@ CamHistogram::CamHistogram(QCam & theCam) :
    sizePolicyMin.setVerData(QSizePolicy::Minimum);
    sizePolicyMin.setHorData(QSizePolicy::Minimum);
    mainWindow_->setSizePolicy(sizePolicyMin);
-   
+
    focusGroup_ = new QVGroupBox(tr("Focus & Seeing"), mainWindow_);
    focusArea_ = new QHistogram(focusGroup_);
    focusArea_->setDataSize(focusHistorySize_);
    focusArea_->setAutoShift(true);
    focusArea_->setAverage(3);
    QToolTip::add(focusArea_,tr("Help to focus. High value implies:\nHigh contrast (sharper image)"));
-   
+
    focusArea_->setMinimumSize(focusHistorySize_,80);
-   //focusArea_->setMaximumSize(focusHistorySize_,300);
    resetFocus_ = new QPushButton(tr("reset"),focusGroup_);
    focusGroup_->setSizePolicy(sizePolicyMin);
    focusArea_->setSizePolicy(sizePolicyMax);
-   
+
    seeingGroup_= new QHBox(focusGroup_);
    seeingLabel_= new QLabel(tr("Seeing: "),seeingGroup_);
    seeingLevel_=  new QProgressBar(seeingGroup_);
    seeingLevel_->setCenterIndicator(true);
    seeingLevel_->setTotalSteps(100);
    QToolTip::add(seeingLevel_,tr("Shown an evaluation of the seeing by looking how\nthe contrast changes between frames."));
-   //seeingValue_ = new QLCDNumber(5,focusGroup_);
-   //seeingValue_->setSmallDecimalPoint(false);
-   
+
    connect(resetFocus_,SIGNAL(pressed()),focusArea_,SLOT(reset()));
    histoGroup_ = new QVGroupBox(tr("Histogram"), mainWindow_);
    histogramArea_ = new QHistogram(histoGroup_);
@@ -71,7 +65,6 @@ CamHistogram::CamHistogram(QCam & theCam) :
    histogramArea_->setAverage(5);
    histogramArea_->displayMode(QHistogram::LogDisplay);
    histogramArea_->setMinimumSize(256,120);
-   //histogramArea_->setMaximumSize(256,300);
    histoGroup_->setSizePolicy(sizePolicyMin);
    histogramArea_->setSizePolicy(sizePolicyMax);
    QToolTip::add(histogramArea_,
@@ -103,7 +96,7 @@ void CamHistogram::newFrame() {
    for (int i=0;i<256;++i) {
       histogram_[i]=0;
    }
-   
+
    for(int i=(wh-1);
        i>=0;--i) {
       ++histogram_[tab[i]];
@@ -112,7 +105,7 @@ void CamHistogram::newFrame() {
      histogramArea_->setValue(histogram_[i],i);
    }
    histogramArea_->update();
-   
+
    int max=0;
    for(int i=(wh-1-w-1);
        i>=0;--i) {
@@ -120,8 +113,7 @@ void CamHistogram::newFrame() {
       distSum+=abs(tab[i+1]-tab[i])+abs(tab[i+w]-tab[i]);
       if (tab[i]>max) max=tab[i];
    }
-   //distSum/=wh*max+1;
-      
+
    focusArea_->setValue(distSum*distSum*distSum);
    focusArea_->update();
 
@@ -139,7 +131,6 @@ void CamHistogram::newFrame() {
 
 double CamHistogram::getDistFromNeibourg(int x,int y) const {
    int ref = cam().getY(x,y);
-   
    return (abs(ref-cam().getY(x+1,y))
            + abs(ref-cam().getY(x,y+1)))/(double)ref;
 }
