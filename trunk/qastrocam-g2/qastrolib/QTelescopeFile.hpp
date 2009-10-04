afc780c9faf9c2362bce850db637cc37c813acaa
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-10-04 12:58:34 +0000

    comments added

--- trunk/qastrocam-g2/qastrolib/QTelescopeFile.hpp
+++ trunk/qastrocam-g2/qastrolib/QTelescopeFile.hpp
@@ -13,29 +13,38 @@ class QTelescopeFile : public QTelescope {
    Q_OBJECT;
 public:
    QTelescopeFile(const char * filePath);
+   // update shitfing value when a new frame comes
    virtual void Update(double x, double y);
+   // creat new files when the tracker is reset
    virtual void Reset();
+   // parent class compatibility
    virtual void setTrack(bool tracking);
-   public slots:
+public slots:
+   // moves
    virtual void goE(float shift){};
    virtual void goW(float shift){};
    virtual void goS(float shift){};
    virtual void goN(float shift){};
+   // stop
    virtual void stopW() {}
    virtual void stopE() {}
    virtual void stopN() {}
    virtual void stopS() {}
+   // speed and track
    virtual double setSpeed(double speed) {return speed; }
    virtual bool setTracking(bool activated) {return true; }
   private:
+   // timing
    double getTime();
    double sessionTime;
    void writeToFile();
+   // files
    string genericFilePath;
    string filePathx;
    string filePathy;
    FILE* descriptorx_;
    FILE* descriptory_;
+   // current datas
    float xPosition;
    float yPosition;
    bool tracking_;
