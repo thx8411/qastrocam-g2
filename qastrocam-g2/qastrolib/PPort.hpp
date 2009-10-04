d9407304dcd4e64865e547d196f9922a857b2f81
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2009-10-04 04:34:23 +0000

    comments added

--- qastrocam-g2/qastrolib/PPort.hpp
+++ qastrocam-g2/qastrolib/PPort.hpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 // ports types :
-
 #define LP_TYPE		0
 #define PPDEV_TYPE	1
 
 #define PPORT_TABLE_SIZE	4
 
+// opened port entry
 typedef struct {
    string name;
    int type;
@@ -19,19 +19,25 @@ typedef struct {
    unsigned char data;
 } pportEntry;
 
+// parallel port access class, singleton
 class PPort {
 private :
    // assuming there is no more than PPORT_TABLE_SIZE ports used at the same time
    pportEntry pportTable[PPORT_TABLE_SIZE];
    int pportTableSize;
+   // singleton class
    static PPort* instance_;
    PPort();
    ~PPort();
+   // find port entry using his name, -1 if not found
    int pportFind(const char* name);
 public:
+   // singleton class
    static PPort* instance();
    static void destroy();
+   // get access to a port, returns entry number, or -1 (error)
    int getAccess(const char* device);
+   // change the port bit using entry number, true if success
    bool setBit(int bit, bool value, int entry);
 };
 #endif
