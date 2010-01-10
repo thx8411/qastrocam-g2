354baaeaf80ecd1876be860219d2c063fdc3c0be
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-01-10 20:39:58 +0000

    FITS adapted for yuv444

--- qastrocam-g2/qastrolib/FitsImage.cpp
+++ qastrocam-g2/qastrolib/FitsImage.cpp
@@ -19,7 +19,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA  02110-1301, USA.
 *******************************************************************/
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -29,18 +28,16 @@ MA  02110-1301, USA.
 #include <fcntl.h>
 #include <math.h>
 #include "FitsImage.hpp"
-
 #include "yuv.hpp"
 
-#define DEFINC 4
-#define DEFDEC 20
+//#define DEFINC 4
+//#define DEFDEC 20
 
 #include "QCamFrame.hpp"
 
 using namespace std;
 
-FitsImageCFITSIO::FitsImageCFITSIO(const string & fileName,
-                                   bool multipleFrame) :
+FitsImageCFITSIO::FitsImageCFITSIO(const string & fileName,bool multipleFrame) :
    FitsImage(fileName),fptr_(NULL),multipleFrame_(multipleFrame) {
    status_=0;
    numAxes_=2;
@@ -56,7 +53,6 @@ bool FitsImageCFITSIO::load(QCamFrame& frame) {
 }
 
 bool FitsImageCFITSIO::initFits(const QCamFrame& frame) {
-
    numAxes_=2;
    axesDim_[0]=frame.size().width();
    axesDim_[1]=frame.size().height();
@@ -64,10 +60,6 @@ bool FitsImageCFITSIO::initFits(const QCamFrame& frame) {
 
    switch (frame.getMode()) {
    case GreyFrame:
-   case RawRgbFrame1:
-   case RawRgbFrame2:
-   case RawRgbFrame3:
-   case RawRgbFrame4:
       axesDim_[2]=1;
       numAxes_=2;
       break;
@@ -77,33 +69,25 @@ bool FitsImageCFITSIO::initFits(const QCamFrame& frame) {
       break;
    break;
    }
-
-   fprintf(stdout,"CreateFITS ");
    fits_create_file(&fptr_,fileName_.c_str(),&status_);
    fits_report_error(stdout,status_);
-
-   fprintf(stdout,"createImage ");
-   fits_create_img(fptr_,BYTE_IMG /* 8 */, numAxes_, axesDim_,&status_);
+   fits_create_img(fptr_,BYTE_IMG, numAxes_, axesDim_,&status_);
    fits_report_error(stdout,status_);
 }
 
 bool FitsImageCFITSIO::save(const QCamFrame& frame) {
 
-   if (fptr_==NULL) {
+   if (fptr_==NULL)
       initFits(frame);
-   }
-   fprintf(stdout,"WriteImage ");
    switch (numAxes_) {
    case 2:
       fits_write_pix(fptr_,TBYTE, base_,axesDim_[0]*axesDim_[1]*axesDim_[2],(void*)frame.Y(),&status_);
       fits_report_error(stdout,status_);
       break;
    case 3:
-   {
-      uchar * colorImageBuff = new uchar[axesDim_[0] * axesDim_[1] * 3];
-      yuv444_to_rgb24(axesDim_[0],axesDim_[1],
-                      frame.Y(),frame.U(),frame.V(),colorImageBuff);
-      uchar * colorImagePlane= new uchar[axesDim_[0] * axesDim_[1]];
+      uchar* colorImageBuff = new uchar[axesDim_[0] * axesDim_[1] * 3];
+      yuv444_to_rgb24(axesDim_[0],axesDim_[1],frame.Y(),frame.U(),frame.V(),colorImageBuff);
+      uchar* colorImagePlane= new uchar[axesDim_[0] * axesDim_[1]];
       for (int i=0;i<3;++i) {
          for(int j=0;j<axesDim_[0] * axesDim_[1];++j) {
             colorImagePlane[j]=colorImageBuff[j*3+i];
@@ -114,23 +98,16 @@ bool FitsImageCFITSIO::save(const QCamFrame& frame) {
       }
       delete colorImageBuff;
       delete colorImagePlane;
-   }
    break;
-   default:
-      assert(0);
    }
-
    return (status_==0);
 }
 
 bool FitsImageCFITSIO::close() {
    if (fptr_ != NULL) {
-      fprintf(stdout,"CloseFITS");
       fits_close_file(fptr_,&status_);
       fits_report_error(stdout,status_);
-      fprintf(stdout,"\n");
       fptr_=NULL;
-      fflush(stdout);
    }
    return (status_==0);
 }
