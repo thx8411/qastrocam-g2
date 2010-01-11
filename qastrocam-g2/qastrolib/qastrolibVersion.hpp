b1782c8e48198a45fbeddde3cbf1c133ae125fa3
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-01-11 22:36:51 +0000

    raw mode bug fixed

--- qastrocam-g2/qastrolib/qastrolibVersion.hpp
+++ qastrocam-g2/qastrolib/qastrolibVersion.hpp
@@ -23,8 +23,8 @@ MA  02110-1301, USA.
 #ifndef _qastrolibVersion_hpp_
 #define _qastrolibVersion_hpp_
 
-const char * const qastrolibVersion ="4.6beta20100111";
-const int          qastrolibBuild   =68;
+const char * const qastrolibVersion ="4.6beta20100112";
+const int          qastrolibBuild   =69;
 const char * const qastrolibWeb = "http://sourceforge.net/projects/qastrocam-g2/";
 const char * const qastrolibMail = "thx8411@users.sourceforge.net";
 const char * const qastrolibName = "QastroLib-g2";
