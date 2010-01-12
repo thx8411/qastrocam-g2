3372dbedfe0b4c5fd082fc02ef9d8c6e328d2ad5
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-01-12 14:47:12 +0000

    QCamAdd adapted to yuv444

--- qastrocam-g2/qastrolib/bayer.cpp
+++ qastrocam-g2/qastrolib/bayer.cpp
@@ -1,6 +1,6 @@
 /******************************************************************
 Qastrocam-g2
-Copyright (C) 2009   Blaise-Florentin Collin
+Copyright (C) 2009-2010 Blaise-Florentin Collin
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License v2
