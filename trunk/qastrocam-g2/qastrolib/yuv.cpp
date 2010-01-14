ed6841722b3aa4bed94f51300efee476021e8dd1
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-01-14 13:22:42 +0000

    added raw 8 bits grey support for AVI

--- trunk/qastrocam-g2/qastrolib/yuv.cpp
+++ trunk/qastrocam-g2/qastrolib/yuv.cpp
@@ -1,6 +1,6 @@
 /******************************************************************
 Qastrocam-g2
-Copyright (C) 2009   Blaise-Florentin Collin
+Copyright (C) 2009-2010 Blaise-Florentin Collin
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License v2
@@ -105,17 +105,6 @@ void yuv444_to_bgr24(int w, int h, const unsigned char* srcY, const unsigned cha
    }
 }
 
-// swap rgb24 (upside down)
-void rgb24_vertical_swap(int w, int h, unsigned char* data){
-   unsigned char* tmp;
-   tmp=(unsigned char*)malloc(w*h*3);
-   memcpy(tmp,data,w*h*3);
-   for(int i=0;i<h;i++) {
-      memcpy(&data[i*w*3],&tmp[(h-i-1)*w*3],w*3);
-   }
-   free(tmp);
-}
-
 // yuv444 planar to rgb32
 void yuv444_to_bgr32(int w, int h, const unsigned char* srcY, const unsigned char* srcU, const unsigned char* srcV, unsigned char* dst){
    int p,size;
@@ -144,4 +133,30 @@ void yuv444_to_yuv420(int w, int h, const unsigned char* srcY, const unsigned ch
          dstV[j*maxX+i]=srcV[(j*w+i)*2];
       }
    }
+}
+
+//
+// TRANSFORM
+//
+
+// swap 8 bit grey (upside down)
+void grey_vertical_swap(int w, int h, unsigned char* data){
+   unsigned char* tmp;
+   tmp=(unsigned char*)malloc(w*h);
+   memcpy(tmp,data,w*h);
+   for(int i=0;i<h;i++) {
+      memcpy(&data[i*w],&tmp[(h-i-1)*w],w);
+   }
+   free(tmp);
+}
+
+// swap rgb24 (upside down)
+void rgb24_vertical_swap(int w, int h, unsigned char* data){
+   unsigned char* tmp;
+   tmp=(unsigned char*)malloc(w*h*3);
+   memcpy(tmp,data,w*h*3);
+   for(int i=0;i<h;i++) {
+      memcpy(&data[i*w*3],&tmp[(h-i-1)*w*3],w*3);
+   }
+   free(tmp);
 }
