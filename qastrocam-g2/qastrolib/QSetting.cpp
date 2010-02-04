90f2db6d1dda323a68f08526c177c6f35eb29771
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-02-04 23:25:03 +0000

    settings gui started

--- qastrocam-g2/qastrolib/QSetting.cpp
+++ qastrocam-g2/qastrolib/QSetting.cpp
@@ -30,10 +30,42 @@ QSetting::~QSetting() {
 
 QWidget *QSetting::buildGUI(QWidget * parent) {
    remoteCTRL_= new QVBox(parent);
+   remoteCTRL_->setSpacing(4);
+
+   // video device box
+   videoBox=new QHGroupBox("Video device",remoteCTRL_);
+   remoteCTRL_->setStretchFactor(videoBox,10);
+
+   // telescope box
+   telescopeBox=new QVGroupBox("Telescope control",remoteCTRL_);
+   remoteCTRL_->setStretchFactor(telescopeBox,10);
+
+   // long exposure box
+   lxBox=new QVGroupBox("Longue exposure",remoteCTRL_);
+   remoteCTRL_->setStretchFactor(lxBox,10);
+
+   // options box
+   optionsBox=new QVGroupBox("Options",remoteCTRL_);
+   remoteCTRL_->setStretchFactor(optionsBox,10);
+
+   // buttons
+   buttonsBox=new QHBox(remoteCTRL_);
+   remoteCTRL_->setStretchFactor(buttonsBox,0);
+   save=new QPushButton("Save",buttonsBox);
+   restore= new QPushButton("Restore",buttonsBox);
+
+   connect(save,SIGNAL(released()),this,SLOT(saveSettings()));
+   connect(restore,SIGNAL(released()),this,SLOT(restoreSettings()));
 
    return(remoteCTRL_);
 }
 
 const QString & QSetting::label() const {
    return label_;
+}
+
+void QSetting::saveSettings() {
+}
+
+void QSetting::restoreSettings() {
 }
