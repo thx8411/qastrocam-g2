49037bcde267277b1c3f6c305df1aa0ae8cefc05
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-02-05 11:50:21 +0000

    camera detection changes

--- trunk/qastrocam-g2/qastrolib/bayer.cpp
+++ trunk/qastrocam-g2/qastrolib/bayer.cpp
@@ -17,6 +17,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA  02110-1301, USA.
 *******************************************************************/
 
+// uses ITU-R formulas
 
 #include "bayer.hpp"
 #include "yuv.hpp"
