363dda6965c066d48b1396849038fb245dcaa75f
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-02-06 03:13:54 +0000

    working on settings gui

--- trunk/qastrocam-g2/qastrolib/QFileChooser.cpp
+++ trunk/qastrocam-g2/qastrolib/QFileChooser.cpp
@@ -17,19 +17,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA  02110-1301, USA.
 *******************************************************************/
 
-//
-// TO BE REWROTE !!!
-//
-
-
 #include "QFileChooser.moc"
 
 #include <qfiledialog.h>
 #include <qtooltip.h>
 
-//#include <libgen.h> // for basename
 #include <stdlib.h>
-//#include <unistd.h> // for get_current_dir_name
+#include <unistd.h>
 
 #include <iostream>
 
@@ -37,16 +31,14 @@ MA  02110-1301, USA.
 
 using namespace std;
 
-QFileChooser::QFileChooser(QWidget * parent):
+QFileChooser::QFileChooser(QWidget * parent, int type):
    QPushButton(parent) {
    QPixmap* tmpIcon;
-   char * curFile=get_current_dir_name();
    connect(this,SIGNAL(pressed()),this,SLOT(selectFile()));
-   setFile(curFile);
-   free(curFile);
    tmpIcon=QCamUtilities::getIcon("choose_directory.png");
    setPixmap(*tmpIcon);
    delete tmpIcon;
+   fileType=type;
 }
 
 QFileChooser::~QFileChooser() {
@@ -54,23 +46,29 @@ QFileChooser::~QFileChooser() {
 
 void QFileChooser::selectFile() {
    setDisabled(true);
-   QString newDir = QFileDialog::getExistingDirectory(
-      currentDir_,
-      this,
-      "get existing directory",
-      "Choose a directory",
-      TRUE );
+   QString newFile;
+
+   if(fileType==DEVICE_FILE) {
+      newFile  = QFileDialog::getOpenFileName(
+         "/dev/",
+         "Devices (*)",
+         this,
+         "get device",
+         "Choose a device");
+   } else {
+      newFile  = QFileDialog::getSaveFileName(
+         get_current_dir_name(),
+         "Devices/Files (*)",
+         this,
+         "get device or set a file name",
+         "Choose a device/file");
+   }
    setDisabled(false);
-   if (!newDir.isEmpty() && !newDir.isNull()) {
-      setFile(newDir);
+   if (!newFile.isEmpty() && !newFile.isNull()) {
+      setFile(newFile);
    }
 }
 
 void QFileChooser::setFile(const QString & fileName) {
-   currentDir_=fileName;
-   char * tmpStr=strdup(fileName.latin1());
-   //setText(basename(tmpStr));
-   free(tmpStr);
-   //QToolTip::add(this, currentDir_);
    emit(fileChanged(fileName));
 }
