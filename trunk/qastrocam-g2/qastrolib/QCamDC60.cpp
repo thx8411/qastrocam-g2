c1483d9d1e6f5f76f493008927c782f9c7783a75
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-02-27 19:05:55 +0000

    long exposure support added for astroeasycap

--- trunk/qastrocam-g2/qastrolib/QCamDC60.cpp
+++ trunk/qastrocam-g2/qastrolib/QCamDC60.cpp
@@ -20,6 +20,9 @@ MA  02110-1301, USA.
 *******************************************************************/
 
 #include <sys/ioctl.h>
+#include <math.h>
+
+#include <qtooltip.h>
 
 #include "QCamDC60.moc"
 
@@ -27,7 +30,6 @@ MA  02110-1301, USA.
 
 QCamDC60::QCamDC60(const char * devpath):
    QCamV4L2(devpath,ioNoBlock|ioUseSelect|haveBrightness|haveContrast|haveHue|haveColor) {
-   resize(QSize(176,144));
 }
 
 void QCamDC60::setGPSW(bool b) {
@@ -37,4 +39,115 @@ void QCamDC60::setGPSW(bool b) {
 
    if(ioctl(device_,VIDIOC_S_CTRL,&ctrl)!=0)
       cout << "Unable to change GPSW state" << endl;
+}
+
+QWidget *  QCamDC60::buildGUI(QWidget * parent) {
+   QWidget * remoteCTRL=QCamV4L2::buildGUI(parent);
+   remoteCTRLlx->hide();
+
+   // extra controls
+   QVGroupBox* extraCtrl=new QVGroupBox("Extra controls",remoteCTRL);
+
+   // long exposure
+   QHGroupBox* lxCtrl=new QHGroupBox("Long exposure",remoteCTRL);
+   QWidget* padding1=new QWidget(lxCtrl);
+   lxCheck=new QCheckBox("Activate",lxCtrl);
+   QWidget* padding2=new QWidget(lxCtrl);
+   lxLabel=new QLabel("Time :",lxCtrl);
+   lxLabel->setEnabled(false);
+   lxEntry=new QLineEdit(lxCtrl);
+   lxEntry->setMaximumWidth(48);
+   lxEntry->setEnabled(false);
+   lxEntry->setText("0.02");
+   lxSet=new QPushButton("Set",lxCtrl);
+   lxSet->setMaximumWidth(32);
+   lxSet->setEnabled(false);
+   QWidget* padding3=new QWidget(lxCtrl);
+   lxProgress= new QProgressBar(lxCtrl);
+   lxProgress->setEnabled(false);
+   QWidget* padding4=new QWidget(lxCtrl);
+
+   QToolTip::add(lxCheck,"Activate long exposure mode");
+   QToolTip::add(lxEntry,"Integration time value");
+   QToolTip::add(lxSet,"Set integration time");
+   QToolTip::add(lxProgress,"Integration progress");
+
+   connect(lxCheck,SIGNAL(stateChanged(int)),this,SLOT(lxActivated(int)));
+   connect(lxSet,SIGNAL(released()),this,SLOT(lxSetPushed()));
+
+   return(remoteCTRL);
+}
+
+// gui slots
+
+void QCamDC60::lxActivated(int b) {
+   if(b==QButton::On) {
+      lxTimer= new QTimer();
+      progressTimer= new QTimer();
+      lxLabel->setEnabled(true);
+      lxEntry->setEnabled(true);
+      lxSet->setEnabled(true);
+      lxProgress->setEnabled(true);
+      // create objects
+      lxTimer= new QTimer();
+      progressTimer= new QTimer();
+      lxControler=new SCmodDC60(*this);
+      // connect
+      connect(progressTimer,SIGNAL(timeout()),this,SLOT(lxProgressStep()));
+      connect(lxTimer,SIGNAL(timeout()),this,SLOT(lxTimeout()));
+   } else {
+      // stop lx
+      // delete objects
+      delete lxControler;
+      delete progressTimer;
+      delete lxTimer;
+      lxLabel->setEnabled(false);
+      lxEntry->setEnabled(false);
+      lxSet->setEnabled(false);
+      lxProgress->setEnabled(false);
+      lxProgress->reset();
+   }
+}
+
+void QCamDC60::lxSetPushed() {
+   float val;
+   lxControler->stopAccumulation();
+   // reading edit line and converts
+   QString str=lxEntry->text();
+   if (sscanf(str.latin1(),"%f",&val)!=1) {
+      // default delay
+      val=0.2;
+   }
+   // main delay
+   if(val<0.2)
+      val=0.2;
+   // 0.2s steps
+   val=round(val*5)/5;
+   lxDelay=val;
+   // progress bar update
+   lxProgress->setTotalSteps((int)(lxDelay/0.2));
+
+   lxProgress->reset();
+   // lx time update
+   lxTimer->stop();
+   lxTimer->start((int)(lxDelay*1000));
+   // progress timer
+   progressTimer->start(200);
+   progress=0;
+   // text update
+   lxEntry->setText(QString().sprintf("%4.2f",lxDelay));
+   setProperty("FrameRateSecond",1.0/lxDelay);
+   lxControler->startAccumulation();
+}
+
+void QCamDC60::lxProgressStep() {
+   progress++;
+   lxProgress->setProgress(progress);
+}
+
+void QCamDC60::lxTimeout() {
+   lxControler->stopAccumulation();
+   usleep(33000);
+   progress=0;
+   lxControler->startAccumulation();
 }
