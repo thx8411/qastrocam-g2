35cb4bc5c8218b51cf705e43292eb1a5e1e0701f
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-04-04 12:03:09 +0000

    comment change

--- qastrocam-g2/qastrolib/bayer.hpp
+++ qastrocam-g2/qastrolib/bayer.hpp
@@ -21,13 +21,13 @@ MA  02110-1301, USA.
 #ifndef _BAYER_HPP_
 #define _BAYER_HPP_
 
-// vesta raw to 8 bits green using red pixels only (source res must be w*2 x h*2)
+// vesta raw to 8 bits green using bilinear interpolation
 void raw2green(unsigned char* Y, unsigned char* data, const int w, const int h, int mode);
 
-// vesta raw to 8 bits red using red pixels only (source res must be w*2 x h*2)
+// vesta raw to 8 bits red using bilinear interpolation
 void raw2red(unsigned char* Y, unsigned char* data, const int w, const int h, int mode);
 
-// vesta raw to 8 bits blue using blue pixels only (source res must be w*2 x h*2)
+// vesta raw to 8 bits blue using bilinear interpolation
 void raw2blue(unsigned char* Y, unsigned char* data, const int w, const int h, int mode);
 
 // vesta raw to yuv444 conversion (nearest)
