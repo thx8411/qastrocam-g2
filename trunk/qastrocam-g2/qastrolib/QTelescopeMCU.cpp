45871d29e85170d6d57ad19b955947105252324a
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-10-12 00:53:46 +0000

    now closing serial port in destructor

--- trunk/qastrocam-g2/qastrolib/QTelescopeMCU.cpp
+++ trunk/qastrocam-g2/qastrolib/QTelescopeMCU.cpp
@@ -2,7 +2,7 @@
 Qastrocam
 Copyright (C) 2003-2009   Franck Sicard
 Qastrocam-g2
-Copyright (C) 2009   Blaise-Florentin Collin
+Copyright (C) 2009-2010   Blaise-Florentin Collin
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License v2
@@ -76,6 +76,10 @@ QTelescopeMCU::QTelescopeMCU(const char * deviceName) :
 
    cout << "Setting mount to guide speed:\n";
    setSpeed('G');
+}
+
+QTelescopeMCU::~QTelescopeMCU() {
+   close(descriptor_);
 }
 
 void QTelescopeMCU::buildGUI(QWidget * parent) {
