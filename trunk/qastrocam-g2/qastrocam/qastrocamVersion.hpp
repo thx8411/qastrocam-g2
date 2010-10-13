714b2c35a47eef351a9c027ac262049a251ddcb7
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-10-13 09:32:23 +0000

    beta update

--- trunk/qastrocam-g2/qastrocam/qastrocamVersion.hpp
+++ trunk/qastrocam-g2/qastrocam/qastrocamVersion.hpp
@@ -23,8 +23,8 @@ MA  02110-1301, USA.
 #ifndef _qastrocamVersion_hpp_
 #define _qastrocamVersion_hpp_
 
-const char * const qastroCamVersion ="4.9beta20101012";
-const int          qastrocamBuild   =129;
+const char * const qastroCamVersion ="4.9beta20101013";
+const int          qastrocamBuild   =130;
 const char * const qastrocamWeb = "http://sourceforge.net/projects/qastrocam-g2/";
 const char * const qastrocamMail = "thx8411@users.sourceforge.net";
 const char * const qastrocamName = "Qastrocam-g2";
