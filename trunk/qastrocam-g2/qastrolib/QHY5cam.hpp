0cd0fc094e29fd749e22b0ffb7f153baaadaabcf
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2010-10-28 19:17:25 +0000

    QHY5 driver cleaned a bit

--- trunk/qastrocam-g2/qastrolib/QHY5cam.hpp
+++ trunk/qastrocam-g2/qastrolib/QHY5cam.hpp
@@ -20,9 +20,14 @@ MA  02110-1301, USA.
 // Interface for the QHY5 native driver
 // singleton class
 
+// Tom's firmware only supports timed moves ont the ST4 port.
+// For permanent moves, this driver use a periodic thread, refreshing
+// the timed moves until stop
+
 #ifndef _QHY5CAM_HPP_
 #define _QHY5CAM_HPP_
 
+// threaded code must be re-entrant
 #ifndef _REENTRANT
 #define _REENTRANT
 #endif
@@ -39,7 +44,7 @@ using namespace std;
 #define	QHY_STOP_NS	5
 #define QHY_STOP_EW	6
 
-// features
+// camera's features
 #define QHY_IMAGER	0
 #define QHY_GUIDER	1
 
@@ -53,11 +58,17 @@ public :
    static void destroy(int feature);
 
    // class functions
+   // reset the cam
    int stop();
+   // start a single shot picture
    int shoot(int duration);
+   // read the last picture shot
    int read(char* image);
+   // ST4 port timed move
    int move(int direction, int duration);
+   // ST4 permanent move
    int move(int direction);
+   // configure the camera
    int configure(int xpos, int ypos, int w, int h, int gg1, int b, int r, int gg2, int* rw, int* rh);
 
    // is the cam plugged ?
@@ -67,30 +78,35 @@ private :
    // functions
    QHY5cam();
    ~QHY5cam();
-   // thread loop
-   static void* callTimedLoop(void* arg) { return(((QHY5cam*)arg)->timedLoop()); }
-   void* timedLoop();
+   // timed moves loop thread
+   static void* callMoveLoop(void* arg) { return(((QHY5cam*)arg)->moveLoop()); }
+   void* moveLoop();
 
-   // vars
+   // singleton vars
    static QHY5cam* instance_;
    static bool feature_used[2];
+
    // usb device
    struct usb_dev_handle* dev;
+
    // configuration
    char* image_;
    int xpos_;
    int ypos_;
    int width_;
    int height_;
+   int size_;
+
    // gains
    int gg1_, bg_, rg_, gg2_;
-   // moves
+
+   // active moves
    bool move_east_, move_west_, move_north_, move_south_;
-   int size_;
-   // timed loop thread
-   pthread_t loop;
-   pthread_mutex_t loopMutex;
-   bool loop_on_;
+
+   // timed moves loop thread
+   pthread_t moveLoopThread;
+   pthread_mutex_t moveLoopMutex;
+   bool moveLoop_on_;
 };
 
 #endif
