2c193cf7ac1aaff88c77017e2f6a397493ca9a27
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2013-03-20 14:39:57 +0000

    kernel version test added

--- qastrolib/qastrolibVersion.hpp
+++ qastrolib/qastrolibVersion.hpp
@@ -2,7 +2,7 @@
 Qastrocam
 Copyright (C) 2003-2009   Franck Sicard
 Qastrocam-g2
-Copyright (C) 2009-2012 Blaise-Florentin Collin
+Copyright (C) 2009-2013 Blaise-Florentin Collin
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License v2
@@ -23,10 +23,15 @@ MA  02110-1301, USA.
 #ifndef _qastrolibVersion_hpp_
 #define _qastrolibVersion_hpp_
 
-const char * const qastrolibVersion ="4.9beta20110812";
-const int          qastrolibBuild   =146;
+const char * const qastrolibVersion ="4.9beta20130320";
+const int          qastrolibBuild   =147;
 const char * const qastrolibWeb = "http://sourceforge.net/projects/qastrocam-g2/";
 const char * const qastrolibMail = "thx8411@users.sourceforge.net";
 const char * const qastrolibName = "QastroLib-g2";
+
+
+// tests the host kernel version
+#include <linux/version.h>
+#define KERNEL_2 (LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,0))
 
 #endif
