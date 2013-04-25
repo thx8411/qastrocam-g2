1a69f16c5a5649ec72e5494478fbfad46aae38d2
thx8411 <thx8411@c1ec87e9-771e-51c6-fe90-2aeb2be771a7>
2013-04-25 20:21:31 +0000

    Q3HBox replacement class added

---
+++ trunk/src/QCamHBox.hpp
@@ -0,0 +1,42 @@
+/******************************************************************
+Qastrocam-g2
+Copyright (C) 2013   Blaise-Florentin Collin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License v2
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA  02110-1301, USA.
+*******************************************************************/
+
+#ifndef _QCAMHBOX_HPP_
+#define _QCAMHBOX_HPP_
+
+#include <Qt/qwidget.h>
+#include <Qt/qboxlayout.h>
+#include <QtCore/qcoreevent.h>
+
+//
+// Replacement for Q3HBox
+//
+
+class QCamHBox: public QWidget {
+   Q_OBJECT
+   public:
+      QCamHBox(QWidget* parent = NULL);
+      ~QCamHBox();
+      // for children detection
+      bool event(QEvent *event);
+   private:
+      QHBoxLayout* widgetLayout;
+};
+
+#endif
